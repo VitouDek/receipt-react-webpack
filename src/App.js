19f539d8f9b529cf1bbd13b949c10afca54fdf55
Dek Panhchavitou <vitou@Vitous-MacBook-Pro.local>
2025-10-08 18:20:27 +0700

    cleaning structure of App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Menu from "./components/Menu";
-
 import data from "./data/recipes";
 
 function App(){
   return(
     <>
-    <Menu recipes={data} />
+      <Menu recipes={data} />
     </>
   )
 }
